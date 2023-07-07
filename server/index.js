e7dcc8d44d341401c195b4f7d6d97cc78b7e1abd
Dev-Radthpon.JS <radthapon747628@gmail.com>
2023-07-08 00:28:44 +0700

    create node server folder and mysql

---
+++ server/index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const mysql = require('mysql2')
+const app = express()
+const port = 3000
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"RadthaponDev",
+    database:"new_books"
+})
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/books', (req, res) => {
+    const q = "SELECT * FROM books"
+    db.query(q, (err, data) => {
+        if (err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+app.post("/books", (req, res) => {
+  const q = "INSERT INTO books (`title_book`,`desc`,`imges_book`) VALUES (?)"
+
+  // const values = ["title from backend","desc from backend","cover from backend"]
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.cover,
+  ]
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err)
+    return res.json("Book has been created successfuly.")
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
