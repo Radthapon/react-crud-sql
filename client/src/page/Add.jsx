6ad8771e22193200f9884c44934f91ee97624dcd
Dev-Radthpon.JS <radthapon747628@gmail.com>
2023-07-11 01:41:54 +0700

    test-my-insert-api

--- client/src/page/Add.jsx
+++ client/src/page/Add.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 
 const Add = () => {
+  const [book, setBook] = useState({
+    title:"",
+    desc:"",
+    price:"",
+    cover:"",
+  })
+
+  const handlerChange = (e) => {
+    setBook(prev=>({...prev,[e.target.name]: e.target.value}));
+  }
+
+  const handlerSubmit = async e => {
+    e.preventDefault()
+    try {
+      await axios.post("http://localhost:3000/books", book)
+    }catch(err){
+      console.log(err);
+    }
+  }
+
   return (
-    <div>Add</div>
+    <div className="add">
+      <h1>ADD NEW BOOK</h1>
+      <input type="text" name="title" placeholder="title book" onChange={handlerChange}/>
+      <input type="text" name="desc" placeholder="desc" onChange={handlerChange}/>
+      <input type="number" name="price" placeholder="price" onChange={handlerChange}/>
+      <input type="text" name="cover" placeholder="cover" onChange={handlerChange}/>
+      <button onClick={handlerSubmit}>Submit</button>
+    </div>
   )
 }
 
